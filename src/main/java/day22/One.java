98ec3c049352e7ea3fdc2d134233a28ae0386c9c
stelar7 <dudedbz@gmail.com>
2019-12-22 15:07:10 +0100

    Day 22

--- src/main/java/day22/One.java
+++ src/main/java/day22/One.java
@@ -1,15 +1,142 @@
 package day22;
 
+import utils.Pair;
 import utils.sources.StringFromFileSupplier;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.*;
 
 public class One
 {
+    enum Action
+    {
+        CUT, REVERSE, DEAL;
+    }
+    
+    static class Card
+    {
+        long id;
+        
+        public Card(long id)
+        {
+            this.id = id;
+        }
+        
+        @Override
+        public String toString()
+        {
+            return "Card{" +
+                   "id=" + id +
+                   '}';
+        }
+    }
+    
     public static void main(String[] args)
     {
-        //IntCodeMachine      machine = new IntCodeMachine("day22.input");
-        List<String> input = StringFromFileSupplier.create("day22.input", false).getDataSource();
+        List<String>                input = StringFromFileSupplier.create("day22.input", false).getDataSource();
+        List<Pair<Action, Integer>> moves = generateMoveList(input);
+        AtomicReference<List<Card>> deck  = new AtomicReference<>(IntStream.range(0, 10007).mapToObj(Card::new).collect(Collectors.toList()));
+        doMoves(moves, deck);
+        findCard(deck, 2019);
+    }
+    
+    public static void findCard(AtomicReference<List<Card>> deck, long cardValue)
+    {
+        List<Card> get = deck.get();
+        for (int i = 0; i < get.size(); i++)
+        {
+            Card card = get.get(i);
+            if (card.id == cardValue)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    
+    public static void doMoves(List<Pair<Action, Integer>> moves, AtomicReference<List<Card>> deck)
+    {
+        moves.forEach(p -> {
+            switch (p.getA())
+            {
+                case CUT:
+                    deck.set(cut(deck.get(), p.getB()));
+                    break;
+                case REVERSE:
+                    deck.set(dealNewStack(deck.get()));
+                    break;
+                case DEAL:
+                    deck.set(dealWithIncrement(deck.get(), p.getB()));
+                    break;
+            }
+        });
+    }
+    
+    private static List<Card> dealWithIncrement(List<Card> cards, int inc)
+    {
+        List<Card> newCards = new ArrayList<>(cards);
+        for (int i = 0; i < cards.size(); i++)
+        {
+            int index = (i * inc) % cards.size();
+            newCards.set(index, cards.get(i));
+        }
+        return newCards;
+    }
+    
+    private static List<Card> cut(List<Card> cards, int cut)
+    {
+        if (cut > 0)
+        {
+            return cutFromTop(cards, cut);
+        }
         
+        return cutFromBottom(cards, cut);
+    }
+    
+    private static List<Card> cutFromBottom(List<Card> cards, int cut)
+    {
+        List<Card> top    = cards.stream().limit(cards.size() + cut).collect(Collectors.toList());
+        List<Card> bottom = cards.stream().skip(cards.size() + cut).collect(Collectors.toList());
+        
+        bottom.addAll(top);
+        return bottom;
+    }
+    
+    private static List<Card> cutFromTop(List<Card> cards, int cut)
+    {
+        List<Card> top    = cards.stream().limit(cut).collect(Collectors.toList());
+        List<Card> bottom = cards.stream().skip(cut).collect(Collectors.toList());
+        
+        bottom.addAll(top);
+        return bottom;
+    }
+    
+    private static List<Card> dealNewStack(List<Card> cards)
+    {
+        List<Card> newStack = new ArrayList<>(cards);
+        Collections.reverse(newStack);
+        return newStack;
+    }
+    
+    public static List<Pair<Action, Integer>> generateMoveList(List<String> input)
+    {
+        return input.stream().map(a -> {
+            if (a.startsWith("cut"))
+            {
+                return new Pair<>(Action.CUT, Integer.parseInt(a.split(" ")[1]));
+            }
+            
+            if (a.startsWith("deal into"))
+            {
+                return new Pair<>(Action.REVERSE, 1);
+            }
+            
+            if (a.startsWith("deal with"))
+            {
+                return new Pair<>(Action.DEAL, Integer.parseInt(a.split(" ")[3]));
+            }
+            
+            return null;
+        }).collect(Collectors.toList());
     }
 }
--- src/main/java/day22/Two.java
+++ src/main/java/day22/Two.java
@@ -1,15 +1,72 @@
 package day22;
 
+import day22.One.Action;
+import utils.Pair;
 import utils.sources.StringFromFileSupplier;
 
+import java.math.BigInteger;
 import java.util.List;
 
 public class Two
 {
     public static void main(String[] args)
     {
-        //IntCodeMachine      machine = new IntCodeMachine("day22.input");
-        List<String> input = StringFromFileSupplier.create("day22.input", false).getDataSource();
+        List<String>                input = StringFromFileSupplier.create("day22.input", false).getDataSource();
+        List<Pair<Action, Integer>> moves = One.generateMoveList(input);
+        System.out.println(solve(moves, 2020, 119315717514047L, -101741582076661L));
+    }
+    
+    public static long solve(List<Pair<Action, Integer>> moves, long target, long cardCount, long iterations)
+    {
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = BigInteger.ZERO;
+        BigInteger m = BigInteger.valueOf(cardCount);
         
+        for (Pair<Action, Integer> move : moves)
+        {
+            if (move.getA() == Action.REVERSE)
+            {
+                a = a.negate().mod(m);
+                b = b.not().mod(m);
+            }
+            
+            if (move.getA() == Action.CUT)
+            {
+                BigInteger count = BigInteger.valueOf(move.getB());
+                b = b.subtract(count).mod(m);
+            }
+            
+            if (move.getA() == Action.DEAL)
+            {
+                BigInteger count = BigInteger.valueOf(move.getB());
+                a = a.multiply(count).mod(m);
+                b = b.multiply(count).mod(m);
+            }
+        }
+        
+        if (iterations < 0)
+        {
+            a = a.modInverse(m);
+            b = b.negate().multiply(a).mod(m);
+        }
+        
+        BigInteger c = BigInteger.ONE;
+        BigInteger d = BigInteger.ZERO;
+        long       e = Math.abs(iterations);
+        
+        while (e > 0)
+        {
+            if ((e & 1) == 1L)
+            {
+                c = a.multiply(c).mod(m);
+                d = a.multiply(d).add(b).mod(m);
+            }
+            
+            e = e >>> 1;
+            b = a.multiply(b).add(b).mod(m);
+            a = a.multiply(a).mod(m);
+        }
+        
+        return BigInteger.valueOf(target).multiply(c).add(d).mod(m).longValue();
     }
 }
